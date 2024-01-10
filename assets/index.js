41474e6bc5577b694b1e839c9e69e3ac7320d34f
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-10 16:51:28 +0100

    SHADOW-MD

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Cipher', 'An Introvert',  'A Noob Web Dev', ' An anime lover', 'A Noob JS Dev'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
